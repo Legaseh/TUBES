d6e9f7a99eb59ccd3d3adeb75fe503f9e643bd8a
Valentino Hose <2472026@maranatha.ac.id>
2025-05-07 09:32:17 +0700

    Scroll taco gerak

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// blm tahu
+
+const model = document.querySelector('.taco3d');
+const model1 = document.querySelector('#taco3d1');
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+
+    // Ubah kamera orbit berdasarkan scroll
+    const theta = scrollY * 0.002;
+    const theta1 = -scrollY * 0.004;
+    const phi = 70; // sudut vertikal tetap
+    const radius = 100; // jarak kamera
+
+    model.cameraOrbit = `${theta}rad ${phi}deg ${radius}m`;
+    model1.cameraOrbit = `${theta1}rad ${phi}deg ${radius}m`;
+});
